5f2a86f690ca3fc5be3b065dedd78e7dffc631d3
zm008 <992683374@qq.com>
2022-06-20 21:28:48 +0800

    dd project1

--- kv/server/raw_api.go
+++ kv/server/raw_api.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"context"
+	"github.com/pingcap-incubator/tinykv/kv/storage"
 	"github.com/pingcap-incubator/tinykv/proto/pkg/kvrpcpb"
 )
 
@@ -11,26 +12,64 @@ import (
 // RawGet return the corresponding Get response based on RawGetRequest's CF and Key fields
 func (server *Server) RawGet(_ context.Context, req *kvrpcpb.RawGetRequest) (*kvrpcpb.RawGetResponse, error) {
 	// Your Code Here (1).
-	return nil, nil
+
+	reader, _ := server.storage.Reader(req.Context)
+	defer reader.Close()
+	b, _ := reader.GetCF(req.Cf, req.Key)
+	return &kvrpcpb.RawGetResponse{
+		Value:    b,
+		NotFound: true,
+	}, nil
 }
 
 // RawPut puts the target data into storage and returns the corresponding response
 func (server *Server) RawPut(_ context.Context, req *kvrpcpb.RawPutRequest) (*kvrpcpb.RawPutResponse, error) {
 	// Your Code Here (1).
 	// Hint: Consider using Storage.Modify to store data to be modified
-	return nil, nil
+
+	put := storage.Put{Cf: req.Cf, Key: req.Key, Value: req.Value}
+
+	modify := storage.Modify{Data: put}
+
+	err := server.storage.Write(req.Context, []storage.Modify{modify})
+
+	return nil, err
 }
 
 // RawDelete delete the target data from storage and returns the corresponding response
 func (server *Server) RawDelete(_ context.Context, req *kvrpcpb.RawDeleteRequest) (*kvrpcpb.RawDeleteResponse, error) {
 	// Your Code Here (1).
 	// Hint: Consider using Storage.Modify to store data to be deleted
-	return nil, nil
+
+	delete := storage.Delete{Cf: req.Cf, Key: req.Key}
+	modify := storage.Modify{Data: delete}
+	err := server.storage.Write(req.Context, []storage.Modify{modify})
+	return nil, err
 }
 
 // RawScan scan the data starting from the start key up to limit. and return the corresponding result
 func (server *Server) RawScan(_ context.Context, req *kvrpcpb.RawScanRequest) (*kvrpcpb.RawScanResponse, error) {
 	// Your Code Here (1).
 	// Hint: Consider using reader.IterCF
-	return nil, nil
+
+	reader, _ := server.storage.Reader(req.Context)
+
+	iterator := reader.IterCF(req.Cf)
+
+	defer reader.Close()
+	defer iterator.Close()
+	limit := req.Limit
+
+	kvs := []*kvrpcpb.KvPair{}
+	for iterator.Seek(req.StartKey); iterator.Valid() && limit > 0; iterator.Next() {
+		//item := iterator.Item()
+
+		//value, _ := item.Value()
+
+		value, _ := iterator.Item().Value()
+		kvs = append(kvs, &kvrpcpb.KvPair{Key: iterator.Item().Key(), Value: value})
+		limit--
+	}
+
+	return &kvrpcpb.RawScanResponse{Kvs: kvs}, nil
 }
